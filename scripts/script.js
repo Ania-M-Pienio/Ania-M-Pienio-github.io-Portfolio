0a9b17c85d0b81375c8496cef2517774462b52a3
Ania-M-Pienio <aniapienio@gmail.com>
2020-03-15 19:03:01 -0400

    nav logo

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,22 @@
 const app = {};
 
+app.animate = function() {
+  // antlers expand on load
+  $(".logo2").addClass("expand");
+  setTimeout(() => {
+      $(".logo3").addClass("expand");
+  }, 1000);
+  // bars expand on load
+  setTimeout(() => {
+    $(".bar").addClass("expand");
+  }, 1200)
+}
+
+app.init = function() {
+  app.animate();
+
+}
 
 $(() => {
-  console.log('app hooked up');
+  app.init();
 })
